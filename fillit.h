7a7217a728052614ddd7a05a1f9e3c87a240c95f
rox <fblin@student.42.fr>
2016-03-28 21:28:07 +0200

    clean

--- fillit.h
+++
@@ -1,6 +0,0 @@
-#ifndef FILLIT_H
-# define FILLIT_H
-# include "libft.h"
-# define EXIT ft_exit("error")
-
-#endif
--- main.c
+++
@@ -1,8 +0,0 @@
-#include "fillit.h"
-
-int		main (int argc, char **argv)
-{
-	if (!(argc == 2))
-		EXIT;
-//	ft_test_input(argv[1]);
-}
